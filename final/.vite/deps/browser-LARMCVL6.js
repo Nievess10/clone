42e3ef04a83c24e36ecbd67bb8997200c2009d34
Martin <martinnievess10@gmail.com>
2024-04-29 10:49:46 -0500

    ran

---
+++ final/.vite/deps/browser-LARMCVL6.js
@@ -0,0 +1,16 @@
+import {
+  Headers,
+  Request,
+  Response,
+  browser_default,
+  fetch
+} from "./chunk-4KPSI3W4.js";
+import "./chunk-UXIASGQL.js";
+export {
+  Headers,
+  Request,
+  Response,
+  browser_default as default,
+  fetch
+};
+//# sourceMappingURL=browser-LARMCVL6.js.map
